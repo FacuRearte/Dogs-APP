ed77079f885a84058af9130ebec084c531c785d6
FacuRearte <Facundorearte13@gmail.com>
2021-08-15 04:52:48 -0300

    1/2

---
+++ api/src/controllers/getApiInfo.js
@@ -0,0 +1,18 @@
+const axios = require( 'axios' );
+
+module.exports = async function getApiInfo() {
+
+    const apiInfo = await axios.get( 'https://api.thedogapi.com/v1/breeds' )
+    const list = await apiInfo.data.map( el => {
+        return {
+            name: el.name,
+            lifeSpan: el.life_span,
+            id: el.id,
+            height: el.height.metric,
+            weight: el.weight.metric,
+            temperament: [el.temperament].join().split(',').map( el => el.trim()),
+            img: el.image.url
+        }
+    })
+    return list;
+}
---
+++ api/src/routes/dogs.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const { Dog } = require("../db");
+// const { Temperament } = require( '../db' );
+const getAllDogs = require("../controllers/getAllDogs");
+
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+  const allDogs = await getAllDogs();
+
+  if (name) {
+    const filtered = allDogs.filter((el) =>
+      el.name.toLowerCase().includes(name.toLowerCase())
+    );
+    if (filtered.length) return res.status(200).send(filtered);
+    return res.status(404).send("The breed of dog has not been found");
+  }
+  return res.status(200).send(allDogs);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (id) {
+    const allDogs = await getAllDogs();
+    const filtered = allDogs.filter((elem) => elem.id == id);
+    if (filtered.length > 0) return res.status(200).send(filtered);
+    return res.status(404).send("The ID was not found");
+  }
+});
+
+router.post("/dog", async (req, res) => {
+  const { name, height, weight, lifeSpan, createdInDb, temperament } = req.body;
+  if (!name || !height || !weight)
+    return res.status(404).send("The name, height and weight are required");
+  const createdDog = await Dog.create({
+    name,
+    height,
+    weight,
+    lifeSpan,
+    createdInDb,
+  });
+  await createdDog.setTemperaments(temperament);
+  return res.status(200).send("The dog has been successfully created");
+});
+
+module.exports = router;
